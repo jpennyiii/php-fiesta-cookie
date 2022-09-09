612a4b23cff43749341f5cfe295463821f5a1953
studyante <cjannpenny@gmail.com>
2022-09-09 21:29:49 +0800

    inc directory done

---
+++ cover-verbose-memory/contacts.php
@@ -0,0 +1,10 @@
+
+<?php include "inc/header.php"; ?>
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Contacts Page.</h1>
+        <p class="lead">Some contact information here..</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+<?php include "inc/footer.php"; ?>
